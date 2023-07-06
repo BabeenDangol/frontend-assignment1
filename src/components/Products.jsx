a24faff326c3569915cdb900232595884a8e81ea
BabeenDangol <104016877+BabeenDangol@users.noreply.github.com>
2023-07-06 17:04:15 +0545

    producedetails

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -29,10 +29,24 @@ const Products = ()=> {
         return(
             <>
             <div className="col-md-6">
-                <img src={product.image} alt={product.title} height="400px" width= "400px"/>
+                <img src={product.image} alt={product.title} 
+                height="400px" width= "400px"/>
             </div>
-            <div className="col-md-6">
-
+            <div className="col-md-6"> 
+            <h4 className="text-uppercase text-black-50">
+              {product.category}
+            </h4>
+            <h1 className="display-5">
+              {product.title}
+            </h1>
+            <p className="lead fw-bolder">
+              Rating {product.rating && product.rating.rate}
+              <i className="fa fa-star"></i>
+            </p>
+            <h3 className="display-6 fw-bold my-4">
+              ${product.price}
+            </h3>
+            <p className="lead">{product.description}</p>
             </div>
             </>
         )
@@ -40,8 +54,10 @@ const Products = ()=> {
   return (
     <div>
         <div className="container">
-
+        <div className="row">
         {loading ? <Loading/> : <ShowProduct/>}
+        </div>
+        
         </div>
     </div>
   )
